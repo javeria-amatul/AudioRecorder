dd7f71acdd3e828c7b6486117daab30aac9e3526
Javeria <javeria@zapr.in>
2019-07-05 19:30:43 +0530

    Improved UI, added spinner in frequency button

--- app/src/main/java/com/redbricklane/zapr/audiorecorder/MainActivity.java
+++ app/src/main/java/com/redbricklane/zapr/audiorecorder/MainActivity.java
@@ -1,5 +1,6 @@
 package com.redbricklane.zapr.audiorecorder;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.media.AudioFormat;
@@ -12,9 +13,13 @@ import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.io.File;
@@ -22,18 +27,18 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-import static android.Manifest.permission.RECORD_AUDIO;
-import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     Context context;
 
     private Button startbtn, stopbtn;
     private              AudioRecord mRecorder;
     private              MediaPlayer mPlayer;
-    private static final String      TAG                           = "AudioRecording";
+    private static final String      TAG                           = "#AUDIO#";
     private static       String      mFileName                     = null;
     public static final  int         REQUEST_AUDIO_PERMISSION_CODE = 1;
     private              int         RECORDER_SAMPLERATE           = 0;
@@ -48,37 +53,55 @@ public class MainActivity extends AppCompatActivity {
     private static final int      RECORDER_BPP                = 16;
     private              EditText lengthOfSample, frequencyOfSample, fileNameTxt;
     private int frequency, lengthInSec, bufferSize;
-    private String fileName;
+    private String             fileName;
+    final   ArrayList<Byte>    audioByteArrayList = new ArrayList<>();
+    private ArrayList<short[]> sampleForASequence = new ArrayList<>();
 
-
+    private String fileId;
+    String audioWavFileName, tempAudioDataFileName;
+    private              FileOutputStream os;
+    private              int[]            samplePerSequence;
+    private              int              seqNum     = 0;
+    private              boolean          skipBuffer = true;
+    private              int              skipItr    = 0;
+    private              Spinner          spinner;
+    private static final String[]         values = {"8000", "16000", "32000" , "44100", "48000"};
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        frequency = 0;
+
         lengthOfSample = (EditText) findViewById(R.id.length);
-        frequencyOfSample = (EditText) findViewById(R.id.frequency);
+//        frequencyOfSample = (EditText) findViewById(R.id.frequency);
         fileNameTxt = (EditText) findViewById(R.id.fileName);
         startbtn = (Button) findViewById(R.id.startRec);
+        spinner = (Spinner)findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+        adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_spinner_item,values);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
         RequestPermissions();
         startbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (CheckPermissions()) {
-                    startbtn.setEnabled(false);
-                    frequency = Integer.parseInt(frequencyOfSample.getText().toString());
+//                    frequency = Integer.parseInt(frequencyOfSample.getText().toString());
                     lengthInSec = Integer.parseInt(lengthOfSample.getText().toString());
                     fileName = fileNameTxt.getText().toString();
-
+                    Toast.makeText(getApplicationContext(), "Recording Started", Toast.LENGTH_LONG).show();
+                    startbtn.setEnabled(false);
                     startRecording();
                     new Handler().postDelayed(new Runnable() {
                         @Override
                         public void run() {
-                            stopRecording();
                             startbtn.setEnabled(true);
                         }
                     }, (lengthInSec * 1000));
-
-                    Toast.makeText(getApplicationContext(), "Recording Started", Toast.LENGTH_LONG).show();
                 } else {
                     RequestPermissions();
                 }
@@ -105,8 +128,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public boolean CheckPermissions() {
-        int result = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
-        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), RECORD_AUDIO);
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO);
         return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
     }
 
@@ -139,85 +162,187 @@ public class MainActivity extends AppCompatActivity {
 
     private void startRecording() {
 
+        audioByteArrayList.clear();
+        Log.i(TAG, "frequency" +frequency);
+
+        try {
+            tempAudioDataFileName = getTempFilename();
+            os = new FileOutputStream(tempAudioDataFileName);
+            audioWavFileName = getFilename();
+        } catch (Exception e) {
+            Log.i(TAG, "Failed to initialize file");
+        }
+
+        int secondsRecorded = 0;
+        int sampleLengthInSeconds = 1 * lengthInSec;
+        int sampleLengthInSecInShort = 0;
+        // The granularity of each chunk is set to 1
+        int sampleLengthForASequence = 1;
+        int sampleLengthInShortForASequence = 0;
+
+        switch (frequency) {
+            case 8000:
+                sampleLengthInSecInShort = sampleLengthInSeconds * 2;
+                sampleLengthInShortForASequence = sampleLengthForASequence * 2;
+                break;
+            case 16000:
+                sampleLengthInSecInShort = sampleLengthInSeconds * 4;
+                sampleLengthInShortForASequence = sampleLengthForASequence * 4;
+                break;
+            case 32000:
+                sampleLengthInSecInShort = sampleLengthInSeconds * 8;
+                sampleLengthInShortForASequence = sampleLengthForASequence * 8;
+                break;
+            case 44100:
+                sampleLengthInSecInShort = sampleLengthInSeconds * 11;
+                sampleLengthInShortForASequence = sampleLengthForASequence * 11;
+                break;
+            case 48000:
+                sampleLengthInSecInShort = sampleLengthInSeconds * 12;
+                sampleLengthInShortForASequence = sampleLengthForASequence * 12;
+                break;
+            default:
+                sampleLengthInSecInShort = sampleLengthInSeconds * 2;
+                sampleLengthInShortForASequence = sampleLengthForASequence * 2;
+                break;
+        }
+
         RECORDER_SAMPLERATE = frequency;
+        samplePerSequence = new int[sampleLengthInSecInShort * 4096];
+
         bufferSize = AudioRecord.getMinBufferSize(
                 RECORDER_SAMPLERATE,
                 RECORDER_CHANNELS,
                 RECORDER_AUDIO_ENCODING);
+        if (bufferSize != AudioRecord.ERROR_BAD_VALUE) {
+            // check if we can instantiate and have a success
+            mRecorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
+                    RECORDER_SAMPLERATE,
+                    RECORDER_CHANNELS,
+                    RECORDER_AUDIO_ENCODING,
+                    8192 * 15);
 
-        mRecorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
-                RECORDER_SAMPLERATE, RECORDER_CHANNELS, RECORDER_AUDIO_ENCODING, 8192 * 15);
+            if (mRecorder.getState() == AudioRecord.STATE_INITIALIZED) {
+                Log.i(TAG, "Recorder initialized & started");
+                mRecorder.startRecording();
+                isRecording = true;
+                int read = 0;
+                sampleForASequence.clear();
+                seqNum = 0;
+                while (true && isRecording) {
+                    short[] buffer = new short[4096];
+                    read = mRecorder.read(buffer, 0, buffer.length);
+                    if (read > 0) {
+                        Log.i(TAG, "buffer value " + skipBuffer);
 
-        int i = mRecorder.getState();
-        if (i == 1)
-            mRecorder.startRecording();
+                        if (skipBuffer) {
+                            if (frequency == 8000) {
+                                Log.i(TAG, "SKIPPING " + seqNum);
+                                skipBuffer = false;
+                                continue;
+                            }
 
-        isRecording = true;
+                        }
+                        Log.i(TAG, Integer.toString(seqNum));
 
-        recordingThread = new Thread(new Runnable() {
+                        if (frequency == 16000) {
+                            if (skipItr < 2) {
+                                Log.i(TAG, "SKIPPING for 16000 " + seqNum);
+                                skipBuffer = false;
+                                skipItr++;
+                                continue;
+                            }
+                        }
 
-            @Override
-            public void run() {
-                writeAudioDataToFile();
-            }
-        }, "AudioRecorder Thread");
+                        if (frequency == 32000 || frequency == 44100) {
+                            if (skipItr < 4) {
+                                Log.i(TAG, "SKIPPING for 32000, 44100 " + seqNum);
+                                skipBuffer = false;
+                                skipItr++;
+                                continue;
+                            }
+                        }
+                        if (frequency == 48000) {
+                            if (skipItr < 8) {
+                                Log.i(TAG, "SKIPPING for 48000 " + seqNum);
+                                skipBuffer = false;
+                                skipItr++;
+                                continue;
+                            }
+                        }
+                        sampleForASequence.add(buffer);
+                        seqNum++;
+                        addBytesToAudioArray(buffer);
+                        buffer = null;
 
-        recordingThread.start();
-    }
+                        if (sampleForASequence.size() >= sampleLengthInShortForASequence) {
+                            Log.i(TAG, "Sample recorded at :  " + System.currentTimeMillis() + "dd/MM/yyyy hh:mm:ss.SSS");
+                            sampleForASequence.clear();
+                            secondsRecorded++;
+                        }
+                        if (seqNum >= sampleLengthInSecInShort) {
+                            Log.i(TAG, "Recording over");
+                            stopRecording();
+                            break;
+                        }
 
-    private void writeAudioDataToFile() {
-
-        byte data[] = new byte[bufferSize];
-        String filename = getTempFilename();
-        FileOutputStream os = null;
-        try {
-            os = new FileOutputStream(filename);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        int read = 0;
-        if (null != os) {
-            while (isRecording) {
-                read = mRecorder.read(data, 0, bufferSize);
-
-                if (AudioRecord.ERROR_INVALID_OPERATION != read) {
-                    try {
-                        os.write(data);
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                    } else {
+                        Log.i(TAG, "Recorder over due to read > 0 is false");
+                        stopRecording();
+                        return;
                     }
                 }
+            } else {
+                stopRecording();
+                Log.i(TAG, "Recorder not initialized");
             }
-
-            try {
-                os.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        } else {
+            Log.i(TAG, "Recording error: Buffer error_bad_value");
         }
+
     }
 
+
     private void stopRecording() {
-        if (null != mRecorder) {
-            isRecording = false;
+        try {
+            if (null != mRecorder) {
+                if (mRecorder.getState() == AudioRecord.STATE_INITIALIZED
+                        && mRecorder.getState() != AudioRecord.RECORDSTATE_STOPPED) {
+                    skipBuffer = true;
+                    isRecording = false;
+                    mRecorder.stop();
+                    mRecorder.release();
+                    mRecorder = null;
+                }
+                mRecorder.release();
 
-            int i = mRecorder.getState();
-            if (i == 1)
-                mRecorder.stop();
-            mRecorder.release();
+                if (audioByteArrayList != null && audioByteArrayList.size() > 0) {
+                    Object[] arrayOfObjects = audioByteArrayList.toArray();
+                    byte[] bytes = new byte[arrayOfObjects.length];
+                    for (int i = 0; i < arrayOfObjects.length; i++) {
+                        bytes[i] = (byte) arrayOfObjects[i];
+                    }
+                    os.write(bytes, 0, bytes.length);
 
-            mRecorder = null;
-            recordingThread = null;
+                    copyWaveFile(tempAudioDataFileName, audioWavFileName);
+                    deleteTempFile();
+                }
+                os.close();
+
+            }
+
+        } catch (Exception | Error ex) {
+            Log.e(TAG, "Error while stopping recording" + ex.getLocalizedMessage());
+            ex.printStackTrace();
         }
-
-        copyWaveFile(getTempFilename(), getFilename());
-        deleteTempFile();
     }
 
     private void deleteTempFile() {
         File file = new File(getTempFilename());
 
         file.delete();
+        Log.i(TAG, "deleteTempFile1");
+
     }
 
     private void copyWaveFile(String inFilename, String outFilename) {
@@ -225,24 +350,30 @@ public class MainActivity extends AppCompatActivity {
         FileOutputStream out = null;
         long totalAudioLen = 0;
         long totalDataLen = totalAudioLen + 36;
+
         long longSampleRate = RECORDER_SAMPLERATE;
         int channels = 1;
-        long byteRate = RECORDER_BPP * RECORDER_SAMPLERATE * channels / 8;
+        long byteRate = RECORDER_SAMPLERATE * 2;
 
-        byte[] data = new byte[bufferSize];
+        byte[] data = new byte[8192 * 14];
+        Log.i(TAG, "copyWaveFile1");
+
 
         try {
             in = new FileInputStream(inFilename);
             out = new FileOutputStream(outFilename);
             totalAudioLen = in.getChannel().size();
             totalDataLen = totalAudioLen + 36;
+            Log.i(TAG, "copyWaveFile2");
 
             WriteWaveFileHeader(out, totalAudioLen, totalDataLen,
                     longSampleRate, channels, byteRate);
 
             while (in.read(data) != -1) {
                 out.write(data);
+
             }
+            Log.i(TAG, "Wav array: " + Arrays.toString(data));
 
             in.close();
             out.close();
@@ -260,6 +391,7 @@ public class MainActivity extends AppCompatActivity {
             long byteRate) throws IOException {
 
         byte[] header = new byte[44];
+        Log.i(TAG, "WriteWaveFileHeader1");
 
         header[0] = 'R';  // RIFF/WAVE header
         header[1] = 'I';
@@ -288,6 +420,8 @@ public class MainActivity extends AppCompatActivity {
         header[24] = (byte) (longSampleRate & 0xff);
         header[25] = (byte) ((longSampleRate >> 8) & 0xff);
         header[26] = (byte) ((longSampleRate >> 16) & 0xff);
+        Log.i(TAG, "WriteWaveFileHeader2");
+
         header[27] = (byte) ((longSampleRate >> 24) & 0xff);
         header[28] = (byte) (byteRate & 0xff);
         header[29] = (byte) ((byteRate >> 8) & 0xff);
@@ -305,13 +439,67 @@ public class MainActivity extends AppCompatActivity {
         header[41] = (byte) ((totalAudioLen >> 8) & 0xff);
         header[42] = (byte) ((totalAudioLen >> 16) & 0xff);
         header[43] = (byte) ((totalAudioLen >> 24) & 0xff);
+        Log.i(TAG, "WriteWaveFileHeader3");
 
         out.write(header, 0, 44);
     }
 
-    private void RequestPermissions() {
-        ActivityCompat.requestPermissions(MainActivity.this, new String[]{RECORD_AUDIO, WRITE_EXTERNAL_STORAGE}, REQUEST_AUDIO_PERMISSION_CODE);
+    private void addBytesToAudioArray(short[] input) {
+        int iterations = input.length;
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(input.length * 2);
+        for (int index = iterations - 1; index > 0; --index) {
+            byteBuffer.putShort(input[index]);
+        }
+
+        byte[] byteArray = byteBuffer.array();
+
+        for (int i = byteArray.length - 1; i >= 0; i--) {
+            audioByteArrayList.add(byteBuffer.get(i));
+        }
     }
 
+    private void RequestPermissions() {
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_AUDIO_PERMISSION_CODE);
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.notifyDataSetChanged();
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        String label = parent.getItemAtPosition(position).toString();
+        int fre = Integer.parseInt(label);
+        Log.i(TAG, "frequency from spinner "+fre);
+
+        switch (fre) {
+            case 8000:
+                frequency = 8000;
+                break;
+            case 16000:
+                frequency = 16000;
+                break;
+            case 32000:
+                frequency = 32000;
+                break;
+            case 44100:
+                frequency = 44100;
+                break;
+            case 48000:
+                frequency = 48000;
+                break;
+            default:
+                frequency = 8000;
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+    }
 }
